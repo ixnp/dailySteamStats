f5c6db5ce809abc28a96831a8b6f2bd3341a0fbe
ixnp <enprocopios@gmail.com>
2017-07-16 12:53:42 -0700

    added jq to check string for users input

--- public/scripts/models/guess.js
+++ public/scripts/models/guess.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // (function (module) {
 //
 // $('#wordGuess').click(funtion(
@@ -8,7 +9,32 @@
 //
 //
 // })(window);
-
+// Uster wants to guess a full word//
 $('#wordGuess').on('click',function(){
-  $('#letter').toggleClass('letter test');
+  $('#guess').toggleClass('letter userWordGuess');
 });
+
+//letter guess//
+var userInput = $('#form');
+var guessArr = [];
+var test = 'test'
+
+userInput.submit(function(e){
+  var userGuess = $('#guess').val();
+  console.log('g1'+userGuess);
+if (test.indexOf(userGuess)!= -1){
+
+    console.log('yes!');
+}
+else{
+  console.log('fuck');
+}
+
+  $('#form')[0].reset();
+  e.preventDefault();
+
+});
+
+
+//word guess if user toggles class//
+ // var userWordGuess = $('#letter').val();
